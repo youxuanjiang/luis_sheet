3c8a5115590277f40d634e7bd749cfcc247cf497
Youxuan Jiang <youxuanjiang19960901@gmail.com>
2021-01-07 01:31:17 +0800

    config seeting

--- config_LUIS.js
+++ config_LUIS.js
@@ -1,10 +1,10 @@
-const LUISauthoringKey = 'modify-here'; // Enter MANAGE->Azure Resource->Authoring Resource
-const LUISappName = 'modify-heret';
-const LUISendpoint = 'modify-here';
+const LUISauthoringKey = 'e34ceb7d5c09400a880e8561855db090';
+const LUISappName = 'NCTU_CAMPUS_QA_INTENTS';
+const LUISendpoint = 'https://westus.api.cognitive.microsoft.com/';
 const LUISappCulture = 'en-us'; // dont need to change
 const LUISversionId = '0.1'; // dont need to change
-const LUISappId = 'modify-here';
-const googleSheetLocation = 'modify here';
+const LUISappId = '791caab4-c4a7-44ba-994f-2b686ef87409';
+const googleSheetLocation = '1JtqlCV2-PuOb-cd7D2QAzS-ao82qqgv6iaw41jCl7l8';
 
 module.exports = {
   LUISauthoringKey,
