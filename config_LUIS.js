7a687627261a456280322cf47c4d39b271a6c03c
youxanjump <mayday19960901@gmail.com>
2021-01-07 01:50:03 +0800

    Update config_LUIS.js

--- config_LUIS.js
+++ config_LUIS.js
@@ -1,10 +1,10 @@
-const LUISauthoringKey = 'e34ceb7d5c09400a880e8561855db090';
-const LUISappName = 'NCTU_CAMPUS_QA_INTENTS';
+const LUISauthoringKey = '-';
+const LUISappName = '-';
 const LUISendpoint = 'https://westus.api.cognitive.microsoft.com/';
 const LUISappCulture = 'en-us'; // dont need to change
 const LUISversionId = '0.1'; // dont need to change
-const LUISappId = '791caab4-c4a7-44ba-994f-2b686ef87409';
-const googleSheetLocation = '1JtqlCV2-PuOb-cd7D2QAzS-ao82qqgv6iaw41jCl7l8';
+const LUISappId = '-';
+const googleSheetLocation = '-';
 
 module.exports = {
   LUISauthoringKey,
