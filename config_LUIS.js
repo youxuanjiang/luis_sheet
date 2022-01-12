2a7743228b8503778c6ce0451aa17454141e5af2
youxanjump <mayday19960901@gmail.com>
2022-01-12 21:07:25 +0800

    Update config_LUIS.js

--- config_LUIS.js
+++ config_LUIS.js
@@ -1,1 +1,41 @@
+const LUISauthoringKey = '-';
+const LUISstagingSlotKey = '-';
+const LUISappName = '-';
+const LUISendpoint = '-';
+const LUISappCulture = 'zh-cn'; // dont need to change
+const LUISversionId = '0.1'; // dont need to change
+const LUISappId = '-';
 
+// const BDRCMssql = {
+//  user: '-',
+//  password: '-',
+//  server: '-',
+//  database: '-',
+//  options: {
+//    encrypt: false,
+//    enableArithAbort: true
+//  }
+// }
+
+const googleSheetForIntent = '-';
+const googleSheetForEntity = '-';
+const googleSheetForTest = '-';
+const googleSheetForAnswer = '-';
+// const googleSheetForAnswer = '1AF78vH-s7RSRaZIwSd2UdBk0227vs5u8VXaiZom-OE4';
+const googleSheetForInformation = '-';
+// const googleSheetForInformation = '13QVuA7srU9YvZgzdwnNFZDkZ5fLgIgznUyfn_Bpwhy4';
+module.exports = {
+ LUISauthoringKey,
+ LUISstagingSlotKey,
+ LUISappName,
+ LUISendpoint,
+ LUISappCulture,
+ LUISversionId,
+ LUISappId,
+ BDRCMssql,
+ googleSheetForIntent,
+ googleSheetForEntity,
+ googleSheetForTest,
+ googleSheetForAnswer,
+ googleSheetForInformation,
+};
